19842543d0d91f637d110d581d603fd8e1601a3d
phantom <623601391@qq.com>
2020-08-27 15:10:54 +0800

    first commit

---
+++ src/main.js
@@ -0,0 +1,44 @@
+const { app, Menu, BrowserWindow, ipcMain } = require('electron')
+
+const dockMenu = Menu.buildFromTemplate([
+  {
+    label: 'New Window',
+    click () { console.log('New Window') }
+  }, {
+    label: 'New Window with Settings',
+    submenu: [
+      { label: 'Basic' },
+      { label: 'Pro' }
+    ]
+  },
+  { label: 'New Command...' }
+])
+
+function createWindow () {   
+  // 创建浏览器窗口
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+
+  // 并且为你的应用加载index.html
+  win.loadFile('index.html')
+}
+
+ipcMain.on('asynchronous-message', (event, arg) => {
+  console.log(arg) // prints "ping"
+  event.reply('asynchronous-reply', 'pong')
+})
+
+ipcMain.on('synchronous-message', (event, arg) => {
+  console.log(arg) // prints "ping"
+  event.returnValue = 'pong'
+})
+
+app.dock.setMenu(dockMenu)
+
+app.whenReady().then(createWindow)
+
